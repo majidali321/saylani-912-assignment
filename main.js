13a6bf37202eb4c930c829502cd98942397fe11e
majidali321 <mianmajid577@gmail.com>
2024-07-07 10:19:39 +0500

    for practice

---
+++ main.js
@@ -0,0 +1,4 @@
+var arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// console.log(arr.indexOf(6));
+console.log(arr.splice(4, 1, 100)); // 0 index se le kr 3 tk values ko urha do or uski jaga 100 ko add kr do
+console.log(arr);
---
+++ main.ts
@@ -0,0 +1,5 @@
+let arr =[1,2,3,4,5,6,7,8]
+// console.log(arr.indexOf(6));
+console.log(arr.splice(0,3,100)); // 0 index se le kr 3 tk values ko urha do or uski jaga 100 ko add kr do
+console.log(arr);
+
